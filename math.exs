6183650705c3da4262657baa1f70fa104be91657
Thomas Chen <chen.thomas.c@gmail.com>
2016-01-29 14:11:58 -0500

    add practice files for getting started guide

--- math.exs
+++
@@ -1,7 +0,0 @@
-defmodule Math do
-  def sum(a, b) do
-    a + b
-  end
-end
-
-IO.puts Math.sum(1, 2)
---
+++ server.ex
@@ -0,0 +1,4 @@
+defmodule MyServer do
+  @initial_state %{host: "147.0.0.1", port: 3456}
+  IO.inspect @initial_state
+end
